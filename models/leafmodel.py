7d5faefc41c2939501f6d103c76d4c08ffe2bed5
nhtlongcs <18120134@student.hcmus.edu.vn>
2020-12-09 19:42:30 +0700

    add baseline vit

--- models/leafmodel.py
+++ models/leafmodel.py
@@ -1,6 +1,8 @@
+import timm
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+
 from .extractors import EfficientNetExtractor
 
 
@@ -23,4 +25,17 @@ class BaseModel(nn.Module):
     def forward(self, x):
         x = self.extractor(x)
         x = self.classifier(x)
+        return x
+
+
+class BaseTimmModel(nn.Module):
+    """Some Information about BaseTimmModel"""
+
+    def __init__(self, num_classes, name="vit_base_patch16_224", freeze_backbone=False):
+        super().__init__()
+        self.model = timm.create_model(name, pretrained=True)
+        self.model.head = nn.Linear(self.model.head.in_features, num_classes)
+
+    def forward(self, x):
+        x = self.model(x)
         return x
