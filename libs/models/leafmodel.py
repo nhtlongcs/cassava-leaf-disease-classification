ce153dc9042c35b665ff3e1ae4a29f05c82f904d
Long Ng <18120134@student.hcmus.edu.vn>
2021-02-16 01:28:40 +0700

    Update leafmodel.py
    add exception for densenet (timm)

--- libs/models/leafmodel.py
+++ libs/models/leafmodel.py
@@ -49,8 +49,10 @@ class BaseTimmModel(nn.Module):
         try:
             self.model.head = nn.Linear(self.model.head.in_features, num_classes)
         except:
-            self.model.fc = nn.Linear(self.model.fc.in_features, num_classes)
-
+            try:
+                self.model.fc = nn.Linear(self.model.fc.in_features, num_classes)
+            except:
+                self.model.classifier = nn.Linear(self.model.classifier.in_features, num_classes)
     def forward(self, x):
         x = self.model(x)
         return x
